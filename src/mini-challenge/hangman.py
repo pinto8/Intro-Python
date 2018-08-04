ffd19dc30f9828594eb801b8a8f8202c24e9719e
Charlie <ccariste@gmail.com>
2018-08-04 10:08:42 -0700

    CRC: most basic errors fixed, now leading with curWord length which is extending by two for some reason...

--- src/mini-challenge/hangman.py
+++ src/mini-challenge/hangman.py
@@ -1,3 +1,4 @@
+import random
 # There are at least 10 "mistakes" in this Hangman game
 # These could include syntax errors, logical errors, spelling errors...
 # Find the mistakes and fix the game!
@@ -15,24 +16,29 @@ bodies = [ " ------\n |    |\n |    O\n |\n |\n |\n |\n |\n---",
 " ------\n |    |\n |  O\n |   \|/\n |    |\n |   / \ \n |\n |\n---" ]
 strikes = 0
 words = [None]
-file = open("word.txt", "r")
+file = open("words.txt", "r")
 for line in file:
     words.append(line)
 file.close()
-targetWord = words[random.randint(0, 100)]
+targetWord = words[random.randint(0, 80)]
 lettersLeft = len(targetWord)-1
+print('letters left', lettersLeft)
 length = len(targetWord)-1
+print('length')
+global curWord
 curWord = "_" * length
-alphabet = [chr(65+x) for x in range(1, 26) ]
+
+alphabet = [chr(65+x) for x in range(0, 26) ]
 
 # Draw body based on # of incorrect guesses
-def drawBody()
+def drawBody():
     print(bodies[strikes])
 
 # Replace blanks with correctly guessed letters
 def fillLetters( letter ):
     for i in range(len(targetWord)-1):
-        if( targetWord[i : i+1]) == letter:
+        if( targetWord[i : i+1]) == letter.lower():
+            global curWord
             curWord = curWord[0: i] + letter + curWord[i: ]
             global lettersLeft
             lettersLeft -= 1
@@ -53,13 +59,15 @@ printWord(alphabet)
 
 # Gameplay loop
 while strikes < 5 and lettersLeft > 0:
-    letter = input("\nPlease guess a letter...")
-    if letter in targetWord:
+    letter = input("\nPlease guess a letter...").upper()
+    if letter not in alphabet:
+        letter = input("\nPlease guess a letter in the alphabet...").upper()
+    elif letter.lower() in targetWord:
         print("Great!")
         fillLetters(letter)
-    else:
+    elif letter in alphabet:
         strikes += 1
-        print( strikes + " / 5 strikes" )
+        print( str(strikes) + " / 5 strikes" )
     printWord(curWord)
     drawBody()
     alphabet.remove(letter.upper())
